9ec5dce1441d921161e8698e6ae350d0353d169c
Erik Chan <erik.chan@gmail.com>
2019-09-24 15:32:31 -0700

    Update adaptive_model.py
    add 'per_token_additional_features'

--- farm/modeling/adaptive_model.py
+++ farm/modeling/adaptive_model.py
@@ -208,6 +208,9 @@ class AdaptiveModel(nn.Module):
                 output = self.dropout(sequence_output)
             elif lm_out == "per_sequence" or lm_out == "per_sequence_continuous":
                 output = self.dropout(pooled_output)
+            elif lm_out == "per_token_additional_features":
+                concat = concat_additional(sequence_output,kwargs["custom_data"])
+                output = self.dropout(concat)                
             elif (
                 lm_out == "per_token_squad"
             ):  # we need a per_token_squad because of variable metric computation later on...
