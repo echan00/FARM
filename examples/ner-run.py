c641f6877e2ee660f861140fda7ae18fcfbd0760
Erik Chan <erik.chan@gmail.com>
2019-10-02 17:48:21 +0800

    Update ner-run.py

--- examples/ner-run.py
+++ examples/ner-run.py
@@ -1,7 +1,5 @@
-
 # fmt: off
 import logging
-
 from farm.data_handler.data_silo import DataSilo
 from farm.data_handler.processor import NER2Processor
 from farm.modeling.optimization import initialize_optimizer
@@ -12,50 +10,40 @@ from farm.modeling.prediction_head import TokenClassificationHead
 from farm.modeling.tokenization import BertTokenizer
 from farm.train import Trainer
 from farm.utils import set_all_seeds, MLFlowLogger, initialize_device_settings
-
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
     datefmt="%m/%d/%Y %H:%M:%S",
     level=logging.INFO,
 )
-
 # ml_logger = MLFlowLogger(tracking_uri="https://public-mlflow.deepset.ai/")
 # ml_logger.init_experiment(experiment_name="Public_FARM", run_name="Run_minimal_example_ner")
-
 ##########################
 ########## Settings
 ##########################
 set_all_seeds(seed=42)
 device, n_gpu = initialize_device_settings(use_cuda=True)
-n_epochs = 3
+n_epochs = 1
 batch_size = 32
-evaluate_every = 500
+evaluate_every = 1000
 lang_model = "bert-base-multilingual-cased"
-
 # 1.Create a tokenizer
 tokenizer = BertTokenizer.from_pretrained(
     pretrained_model_name_or_path=lang_model, do_lower_case=False
 )
-
 # 2. Create a DataProcessor that handles all the conversion from raw text into a pytorch Dataset
-ner_labels = ['[PAD]','X','0','1']
-
+ner_labels = ['[PAD]','X','N','Y','[SEP]','[CLS]']
 processor = NER2Processor(
-    tokenizer=tokenizer, max_seq_len=448, data_dir="./", metric="seq_f1", label_list=ner_labels
+    tokenizer=tokenizer, max_seq_len=384, data_dir="./", metric="seq_f1", label_list=ner_labels
 )
-
 # 3. Create a DataSilo that loads several datasets (train/dev/test), provides DataLoaders for them and calculates a few descriptive statistics of our datasets
 data_silo = DataSilo(processor=processor, batch_size=batch_size)
-
 # 4. Create an AdaptiveModel
 # a) which consists of a pretrained language model as a basis
 language_model = Bert.load(lang_model)
 # b) and a prediction head on top that is suited for our task => NER
-
 custom_feature_dim = 1
 prediction_head = TokenClassificationHead(task_name="ner",
                                           layer_dims=[768+custom_feature_dim, len(processor.tasks["ner"]["label_list"])])
-
 model = AdaptiveModel(
     language_model=language_model,
     prediction_heads=[prediction_head],
@@ -63,7 +51,6 @@ model = AdaptiveModel(
     lm_output_types=["per_token_additional_features"],
     device=device,
 )
-
 # 5. Create an optimizer
 optimizer, warmup_linear = initialize_optimizer(
     model=model,
@@ -72,7 +59,6 @@ optimizer, warmup_linear = initialize_optimizer(
     n_batches=len(data_silo.loaders["train"]),
     n_epochs=n_epochs,
 )
-
 # 6. Feed everything to the Trainer, which keeps care of growing our model into powerful plant and evaluates it from time to time
 trainer = Trainer(
     optimizer=optimizer,
@@ -83,16 +69,12 @@ trainer = Trainer(
     evaluate_every=evaluate_every,
     device=device,
 )
-
 # 7. Let it grow
 model = trainer.train(model)
-
 # 8. Hooray! You have a model. Store it:
 save_dir = "saved_models/bert-tagatag"
 model.save(save_dir)
 processor.save(save_dir)
-
-
 # 9. Load it & harvest your fruits (Inference)
 # basic_texts = [
 #     {"text": "Schartau sagte dem Tagesspiegel, dass Fischer ein Idiot sei"},
